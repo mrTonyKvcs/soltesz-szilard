f7ce144cca9d7762a1eaaa16f169e3d743fdead1
Tony <attila.kovacs92@gmail.com>
2018-12-06 11:39:54 +0100

    email tender view

--- resources/views/emails/tender.blade.php
+++ resources/views/emails/tender.blade.php
@@ -5,7 +5,7 @@
 	<title>Jelentkezes</title>
 </head>
 <body>
-    <strong>{{ $data->title }}</strong>
+    <strong>{{ $data->title }}</strong> <br><br><hr>
 	<strong>Név: </strong> {{ $data->name }}<br><br>
 	<strong>Email: </strong> {{ $data->email }}<br><br>
 	<strong>Telefonszam: </strong> {{ $data->phone_number }}<br><br>
