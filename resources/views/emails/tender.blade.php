4f1e94d3826096c205891eba6cdb1f2ea6538403
Tony <attila.kovacs92@gmail.com>
2018-12-06 11:44:46 +0100

    email tender view change text

--- resources/views/emails/tender.blade.php
+++ resources/views/emails/tender.blade.php
@@ -5,7 +5,7 @@
 	<title>Jelentkezes</title>
 </head>
 <body>
-    <strong>{{ $data->title }}</strong> <br><br><hr>
+    <strong>Esemény neve:{{ $data->title }}</strong> <br><br><hr>
 	<strong>Név: </strong> {{ $data->name }}<br><br>
 	<strong>Email: </strong> {{ $data->email }}<br><br>
 	<strong>Telefonszam: </strong> {{ $data->phone_number }}<br><br>
