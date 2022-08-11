7e6a379faf9ac8ee7ebab1fba2ca85357d67c29a
KovacsJ92 <jozsef.kovacs92@gmail.com>
2022-08-11 18:07:53 +0200

    create about pages

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -6,21 +6,43 @@
             <div class="card-block-big">
                 <h1 class="color-primary">Magamról</h1><hr>
                 <p>
-                    Alapvégzettségem közgazdász, hosszú évekig a kereskedelem és az értékesítés területein dolgoztam vezetőként. Sok hibázásom és csalódásom számos mélyre mutató kérdést hívott elő. Válaszok hiányában magyarázatokat és inspiráló embereket kerestem. 
+                    Alapvégzettségem közgazdász, hosszú évekig a kereskedelem és az értékesítés területein
+                    dolgoztam vezetőként. Sok hibázásom és csalódásom számos mélyre mutató kérdést hívott
+                    elő. Válaszok hiányában magyarázatokat és inspiráló embereket kerestem. 
                 </p>
 
                 <img src="/img/profile1.jpg " alt=" " class="img-responsive imgborder ml-2 mb-2 pull-right ">
                 <p>
-                    2013-ban kezdtem el tanulni a coach-tanácsadást. Kerestem azokat a képzéseket, ahol holisztikus szemlélet van, ahol fontos szempont megismerni az emberi működést, és ahol nem fél igazságokról beszélnek. 2015-ben jelentkeztem a Károli Gáspár Református Egyetem mentálhigiénés segítő szakember képzésére. A képzés vezetői és több oktatója hittek abban, hogy megtalálom azt az igazságot, amit oly rendíthetetlenül keresek. Hivatásom alappillére lett a személyközpontú megközelítés, amivel akaratlanul átitatódtam általuk. Szakmai önbecsülésemet erősítette tanulmányaim következő állomása, az egyetem szupervizor képzése.
+                    2013-ban kezdtem el tanulni a coach-tanácsadást. Kerestem azokat a képzéseket, ahol
+                    holisztikus szemlélet van, ahol fontos szempont megismerni az emberi működést, és ahol nem
+                    fél igazságokról beszélnek. 2015-ben jelentkeztem a Károli Gáspár Református Egyetem
+                    mentálhigiénés segítő szakember képzésére. A képzés vezetői és több oktatója hittek abban,
+                    hogy megtalálom azt az igazságot, amit oly rendíthetetlenül keresek. Hivatásom alappillére
+                    lett a személyközpontú megközelítés, amivel akaratlanul átitatódtam általuk. Szakmai
+                    önbecsülésemet erősítette tanulmányaim következő állomása, az egyetem szupervizor
+                    képzése.
                 </p>
                 <p>
-                    Az emberi lét egyik lényegében, a fejlődésben segítek. Ezért fontos, hogy a szakmai ismeretek mellett, hogyan és milyen eszközökkel készítem fel magamat napi szinten erre a szolgálatra. 
+                    Az emberi lét egyik lényegében, a fejlődésben segítek. Ezért fontos, hogy a szakmai
+                    ismeretek mellett, hogyan és milyen eszközökkel készítem fel magamat napi szinten erre a
+                    szolgálatra.
                 </p>
                 <p>
-                    Inspiráló szakemberek segítségével képzem magamat. Több terápiás eszközt és segítői módszert sajátítottam már el és integráltam a munkámba. Különböző hagyományokkal és filozófiai irányzatokkal ismerkedem, és a teljességre törekvés szándékával kapcsolom össze a tudomány és a spiritualitás világ válaszait. Fontos premisszának tekintem, hogy mások megismerése önmagunk megismerésén keresztül lehetséges. A szakmai, szellemi és spirituális fejlődés hármasa ma már életmód számomra. 
+                    Inspiráló szakemberek segítségével képzem magamat. Több terápiás eszközt és segítői
+                    módszert sajátítottam már el és integráltam a munkámba. Különböző hagyományokkal és
+                    filozófiai irányzatokkal ismerkedem, és a teljességre törekvés szándékával kapcsolom össze a
+                    tudomány és a spiritualitás világ válaszait. Fontos premisszának tekintem, hogy mások
+                    megismerése önmagunk megismerésén keresztül lehetséges. A szakmai, szellemi és spirituális
+                    fejlődés hármasa ma már életmód számomra. 
                 </p>
                 <p>
-                    Azok a szellemi és bölcseleti rendszerek érdekelnek elsősorban, amelyek a változással való együttműködésről szólnak, amelyek gyakorlása által képessé válhatok más gondolkodási funkciókat aktiválni. A hangsúly a tudatosság bővítésére, az erő és az energia hatékony visszafordítására kerül. A változást nem mi hozzuk létre és nem ragadható meg pontosan. Kívülről érkezik, de belülről kerülhetünk valódi kapcsolatba a külső feltételekkel. Az önfejlesztésem része, hogy harcművészeteket gyakorlok, tanulom a figyelmet és a jelenlétet. 
+                    A tudományosság mellett azok a szellemi és bölcseleti rendszerek érdekelnek elsősorban,
+                    amelyek a változással való együttműködésről szólnak, valamint finomítják a
+                    lényegérzékenységet és az információkon alapuló intitív gondolkodást. A változást nem mi
+                    hozzuk létre és nem ragadható meg pontosan. Kívülről érkezik, de belülről kerülhetünk valódi
+                    kapcsolatba a külső feltételekkel. Az önfejlesztésem része, hogy harcművészeteket gyakorlok.
+                    Arra tanínatak, hogy úgy legyek ura önmagamnak, hogy bárhol is legyek, legyek ott teljesen,
+                    és közben maradjak hű önmagamhoz. 
                 </p>
 
             </div>
