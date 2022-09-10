3a87460add63bbca1dfcbc8f1e117656570d2555
tony <attila.kovacs92@gmail.com>
2022-09-10 11:57:26 +0200

    add new version

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -5,7 +5,7 @@
     <div class="row bg-light">
         <div class="container">
             <div class="col-md-12">
-                <h1 class="text-secondary text-uppercase text-center py-5">selfnesscoaching.hu admin</h1>
+                <h1 class="text-secondary text-uppercase text-center py-5">solteszszilard.hu admin</h1>
             </div>
         </div>
     </div>
